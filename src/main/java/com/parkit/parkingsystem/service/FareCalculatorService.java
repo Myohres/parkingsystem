58082d688a3bffeac56800c884a484f61f7b0e6f
Myohres <77498044+Myohres@users.noreply.github.com>
2021-05-05 17:21:33 +0200

    conversion time units fort succes test

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,12 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        float inHour =  (ticket.getInTime().getHours() * 60) + (ticket.getInTime().getMinutes()) ;
+        float outHour = (ticket.getOutTime().getHours() * 60) + (ticket.getOutTime().getMinutes());
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        float durationDay= ( (ticket.getOutTime().getTime()) - (ticket.getInTime().getDay()) )*24;
+        float duration = durationDay + (outHour - inHour)/60;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
