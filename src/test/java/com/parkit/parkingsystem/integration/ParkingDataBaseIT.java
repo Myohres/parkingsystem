fd2767d166e5a73848aedc2bff59b0b9089165f6
Myohres <77498044+Myohres@users.noreply.github.com>
2021-05-13 02:35:55 +0200

    WIP, connection BDD and get id / parkingSpot values

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,7 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import com.parkit.parkingsystem.config.DataBaseConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -51,7 +59,25 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+
+        DataBaseConfig dataBaseConfig = new DataBaseConfig();
+        Connection con = null;
+        try {
+            con = dataBaseConfig.getConnection(); // connection BDD
+            PreparedStatement ps= con.prepareStatement(DBConstants.GET_TICKET); // Connection ticket
+            //
+            // idTicket = id du ticket de la base de donnée
+            // int id= ps.getInt("id");
+
+            // parkingSpotCare = availability de la place de parking de la bdd
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+
+        // assert (ticket base donné vaut 1 && parkingSpotCar = 0)
     }
 
     @Test
