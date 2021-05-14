99d3890457a9f7fcbce592c42409cd3c62b06bfc
Myohres <77498044+Myohres@users.noreply.github.com>
2021-05-14 14:05:47 +0200

    Test ok

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,11 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.constants.DBConstants;
+
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
-import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -15,13 +16,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import com.parkit.parkingsystem.config.DataBaseConfig;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -60,29 +59,15 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
 
-        DataBaseConfig dataBaseConfig = new DataBaseConfig();
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection(); // connection BDD
-            PreparedStatement ps= con.prepareStatement(DBConstants.GET_TICKET); // Connection ticket
-            //
-            // idTicket = id du ticket de la base de donnée
-            // int id= ps.getInt("id");
+        Ticket ticketTestParkingACar = ticketDAO.getTicket("ABCDEF");
+        int parkingSpotTest = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
 
-            // parkingSpotCare = availability de la place de parking de la bdd
-
-        } catch (SQLException | ClassNotFoundException throwables) {
-            throwables.printStackTrace();
-        }
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-
-
-        // assert (ticket base donné vaut 1 && parkingSpotCar = 0)
+        assertNotNull(ticketTestParkingACar);
+        assertEquals(2,parkingSpotTest);
     }
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
