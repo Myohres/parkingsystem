3f227d625975cf70518aabd0b7c1eea6dffcf338
Myohres <77498044+Myohres@users.noreply.github.com>
2021-05-20 05:25:18 +0200

    given ok when ok then WIP

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,11 +1,15 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+import junit.framework.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +17,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -56,9 +63,22 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
+
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
         parkingService.processExitingVehicle();
+
+        Ticket ticketTestParkingExit = ticketDAO.getTicket("ABCDEF");
+        double price = ticketTestParkingExit.getPrice();
+        long inTimeTestParkingExit = ticketTestParkingExit.getInTime().getTime();
+        long outTimeTestParkingExit = ticketTestParkingExit.getOutTime().getTime();
+        long timeSpent = (outTimeTestParkingExit - inTimeTestParkingExit)/1000/60/60;
+        int AvailableSlotTestParkingExit = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        assertTrue(inTimeTestParkingExit < outTimeTestParkingExit);
+        assertEquals(1,AvailableSlotTestParkingExit);
+        assertEquals(price,(timeSpent* Fare.CAR_RATE_PER_HOUR));
+
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
