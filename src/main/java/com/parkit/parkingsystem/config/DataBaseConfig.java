029b7d22e5784c025d4ce18c766618cbc8f793cf
Myohres <77498044+Myohres@users.noreply.github.com>
2021-06-22 19:44:39 +0200

    Security

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -3,48 +3,101 @@ package com.parkit.parkingsystem.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 
 public class DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    /**
+     * Log parameter.
+     */
+    private static final Logger LOGGER = LogManager.getLogger("DataBaseConfig");
+    private static final String DB_LOG_CONNEXION = "config.properties";
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod?useTimezone=true&serverTimezone=UTC","root","rootroot");
+    /**
+     * Connection to DB parking System.
+     * @return parameters connection to DB
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connection getConnection()
+            throws ClassNotFoundException, SQLException {
+
+        FileInputStream fis = null;
+        Properties dbProperties = new Properties();
+        String host;
+        String login;
+        String password;
+        Connection con = null;
+
+        try {
+            fis = new FileInputStream(DB_LOG_CONNEXION);
+            dbProperties.load(fis);
+            host = dbProperties.getProperty("db.prod.host");
+            login = dbProperties.getProperty("db.prod.login");
+            password = dbProperties.getProperty("db.prod.password");
+            LOGGER.info("Create DB connection");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(host, login, password);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error Reading" + DB_LOG_CONNEXION + "file");
+        } catch (IOException e) {
+            System.err.println("Error load Properties from" + DB_LOG_CONNEXION);
+        } finally {
+            try { if (fis != null) { fis.close(); } } catch (IOException e) { }
+        }
+        return con;
     }
 
-    public void closeConnection(Connection con){
-        if(con!=null){
+    /**
+     * Close the DB Parking System Connection.
+     * @param con  Connection con
+     */
+    public void closeConnection(final Connection con) {
+        if (con != null) {
             try {
                 con.close();
-                logger.info("Closing DB connection");
+                LOGGER.info("Closing DB connection");
             } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
+                LOGGER.error("Error while closing connection", e);
             }
         }
     }
 
-    public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
+    /**
+     * Close the PreparedStatement.
+     * @param ps PreparedStatement ps
+     */
+    public void closePreparedStatement(final PreparedStatement ps) {
+        if (ps != null) {
             try {
                 ps.close();
-                logger.info("Closing Prepared Statement");
+                LOGGER.info("Closing Prepared Statement");
             } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
+                LOGGER.error("Error while closing prepared statement", e);
             }
         }
     }
 
-    public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
+    /**
+     * Close the ResultSet.
+     * @param rs ResultSet rs
+     */
+    public void closeResultSet(final ResultSet rs) {
+        if (rs != null) {
             try {
                 rs.close();
-                logger.info("Closing Result Set");
+                LOGGER.info("Closing Result Set");
             } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
+                LOGGER.error("Error while closing result set", e);
             }
         }
     }
