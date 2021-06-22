6d4fa8ff6ed7825a74f54574ceb82bff8d0f2863
Myohres <77498044+Myohres@users.noreply.github.com>
2021-06-22 22:42:10 +0200

    checkStyle Fare

--- src/main/java/com/parkit/parkingsystem/constants/Fare.java
+++ src/main/java/com/parkit/parkingsystem/constants/Fare.java
@@ -1,9 +1,28 @@
 package com.parkit.parkingsystem.constants;
 
 public class Fare {
+    /**
+     * default constructor.
+     */
+    public void constructor() { }
+    /**
+     * Constant Rate for bike per hour.
+     */
     public static final double BIKE_RATE_PER_HOUR = 1.0;
+    /**
+     * Constant Rate for car per hour.
+     */
     public static final double CAR_RATE_PER_HOUR = 1.5;
+    /**
+     * Constant Rate for vehicle stay less 30 minutes.
+     */
     public static final double VEHICLE_RATE_FOR_LESS_HALF_HOUR = 0;
+    /**
+     * Constant Rate for vehicle came before.
+     */
     public static final double RECURRENT_USER_RATE = 0.95;
+    /**
+     * Constant for time free price.
+     */
     public static final double FREE_DURATION = 0.5;
 }
