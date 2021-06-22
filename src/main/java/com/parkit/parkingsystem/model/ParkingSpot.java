8505775575a07cc9336a4bdd4183d444adfe4a23
Myohres <77498044+Myohres@users.noreply.github.com>
2021-06-23 00:19:56 +0200

    checkStyle ParkingSpot

--- src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
+++ src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
@@ -3,48 +3,101 @@ package com.parkit.parkingsystem.model;
 import com.parkit.parkingsystem.constants.ParkingType;
 
 public class ParkingSpot {
+    /**
+     * number parking spot.
+     */
     private int number;
+    /**
+    * type of vehicle.
+     */
     private ParkingType parkingType;
+    /**
+     * availability of parkingSpot.
+     */
     private boolean isAvailable;
 
-    public ParkingSpot(int number, ParkingType parkingType, boolean isAvailable) {
-        this.number = number;
-        this.parkingType = parkingType;
-        this.isAvailable = isAvailable;
+    /**
+     * ParkingSpot constructor.
+     * @param numberInput int
+     * @param parkingTypeInput ParkingType
+     * @param isAvailableInput boolean
+     */
+    public ParkingSpot(final int numberInput,
+                       final ParkingType parkingTypeInput,
+                       final boolean isAvailableInput) {
+        this.number = numberInput;
+        this.parkingType = parkingTypeInput;
+        this.isAvailable = isAvailableInput;
     }
 
+    /**
+     * Get ParkingSpot ID.
+     * @return number ID
+     */
     public int getId() {
         return number;
     }
 
-    public void setId(int number) {
-        this.number = number;
+    /**
+     * Set ParkingSpot ID.
+     * @param numberInput ID
+     */
+    public void setId(final int numberInput) {
+        this.number = numberInput;
     }
 
+    /**
+     * get Parking Type of vehicle.
+     * @return parkingType
+     */
     public ParkingType getParkingType() {
         return parkingType;
     }
 
-    public void setParkingType(ParkingType parkingType) {
-        this.parkingType = parkingType;
+    /**
+     * set Parking Type of vehicle.
+     * @param parkingTypeInput ParkingType
+     */
+    public void setParkingType(final ParkingType parkingTypeInput) {
+        this.parkingType = parkingTypeInput;
     }
 
+    /**
+     * get availability parkingSpot.
+     * @return boolean isAvailable
+     */
     public boolean isAvailable() {
         return isAvailable;
     }
 
-    public void setAvailable(boolean available) {
+    /**
+     * set availability parkingSpot.
+     * @param available boolean
+     */
+    public void setAvailable(final boolean available) {
         isAvailable = available;
     }
 
+    /**
+     *
+     * @param o Object
+     * @return number
+     */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         ParkingSpot that = (ParkingSpot) o;
         return number == that.number;
     }
 
+    /**
+     * @return number
+     */
     @Override
     public int hashCode() {
         return number;
